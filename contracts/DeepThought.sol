8be614c9cc5a0977239de320537fc2b6883e5204
Golfetto Thomas <tgolfetto@alpenite.com>
2021-05-03 23:53:00 +0200

    Solidity v0.8 for safemath, random number function

--- contracts/DeepThought.sol
+++ contracts/DeepThought.sol
@@ -1,12 +1,12 @@
-pragma solidity >=0.7.0 <0.9.0;
-//import 'openzeppelin-solidity/contracts/math/SafeMath.sol';
+pragma solidity >=0.8.0 <0.9.0;
+
+//import "github.com/provable-things/ethereum-api/blob/master/oraclizeAPI_0.5.sol";
 
 /**
  * @title Oracle
  * @dev 
  */
-contract Oracle {
-//    using SafeMath for uint;
+contract Oracle /*is usingOraclize*/ {
     
     enum VoteOption {Unknown, True, False}
     
@@ -51,6 +51,8 @@ contract Oracle {
     mapping (address => uint) balances;
     
     mapping (uint256 => Proposition) propositions;
+
+    uint numPropositions;
     
     uint256[] proposition_list;
     
@@ -70,8 +72,13 @@ contract Oracle {
     function submit_proposition(uint256 _prop_id, bytes32 _prop_content, uint256 _bounty) public {
         require (_bounty > min_bounty, "Bounty is too low, check the minimum bounty");
         require (balances[msg.sender] >= _bounty, "Not enough money to submit");
-        // TODO 
-        propositions[_prop_id] = Proposition(_prop_id, _prop_content, _bounty, VoteOption.Unknown, 0);
+        Proposition storage p = propositions[numPropositions++];
+        p.id =_prop_id;
+        p.content = _prop_content;
+        p.bounty = _bounty;
+        p.decision = VoteOption.Unknown;
+        p.stakes_total = 0;
+        p.num_voters = 0;
     }
     
     function certification_request(uint _stake) public {
@@ -97,7 +104,8 @@ contract Oracle {
         uint stake = certifing_stakes[msg.sender];
         certifing_stakes[msg.sender] = 0;
         prop.certificates[_vote] += 1;
-        _vote ? prop.true_pool += stake : prop.false_pool += stake;
+        //TODO
+        //_vote ? prop.true_pool += stake : prop.false_pool += stake;
     }
     
     function voting_request(uint _stake) public returns (uint256) {
@@ -117,7 +125,7 @@ contract Oracle {
         voting_stakes[msg.sender][_prop_id] = 0;
         Proposition storage prop = propositions[_prop_id];
         prop.votes[_vote] += vote;
-        prop.vote_stakes_total += stake; //TODO
+        //prop.vote_stakes_total += stake; //TODO
         if (prop.num_voters >= max_voters){
             close_proposition(_prop_id);
         }
@@ -141,19 +149,71 @@ contract Oracle {
     }
     
     function normalize_vote_weight(uint _stake, uint reputation) internal pure returns(uint) {
-        return _stake*reputation/0.8;
+        return _stake*reputation/8;
         //TODO something better
     }
     
-    function get_proposition() internal returns(uint256) {
-        require(proposition_list.lenght > 0, "No propositions available");
+    function get_proposition() internal view returns(uint256) {
+        require(numPropositions > 0, "No propositions available");
         return proposition_list[0];
         //TODO make it a real random
     }
     
-    function get_certifier_reward(uint256 _prop_id, address _certifier, bool _outcome) internal returns(uint256){
+    function get_certifier_reward(uint256 _prop_id, address _certifier, bool _outcome) internal view returns(uint256){
         Proposition storage proposition = propositions[_prop_id];
         uint256 stake = proposition.certifier_stakes[_certifier][_outcome];
         return stake;
     }
+
+    function random() private view returns (uint8) 
+    {
+        // real solution is to ask Oraclize but it costs $$$
+        return uint8(uint256(keccak256(abi.encodePacked(block.difficulty)))%251);
+    }
+
+    /*
+    uint256 constant MAX_INT_FROM_BYTE = 256;
+    uint256 constant NUM_RANDOM_BYTES_REQUESTED = 7;
+
+    event LogNewProvableQuery(string description);
+    event generatedRandomNumber(uint256 randomNumber);
+
+    constructor()
+        public
+    {
+        oraclize_setProof(proofType_Ledger);
+        update();
+    }
+
+    function __callback(
+        bytes32 _queryId,
+        string memory _result,
+        bytes memory _proof
+    )
+        public
+    {
+        require(msg.sender == oraclize_cbAddress());
+        if (oraclize_randomDS_proofVerify__returnCode(_queryId, _result, _proof) != 0) {
+            //The proof verification has failed! Handle this case
+        } else {
+            //The proof verifiction has passed!
+            uint256 ceiling = (MAX_INT_FROM_BYTE ** NUM_RANDOM_BYTES_REQUESTED) - 1;
+            uint256 randomNumber = uint256(keccak256(abi.encodePacked(_result))) % ceiling;
+            emit generatedRandomNumber(randomNumber);
+        }
+    }
+
+    function update()
+        payable
+        public
+    {
+        uint256 QUERY_EXECUTION_DELAY = 0; // NOTE: The datasource currently does not support delays > 0!
+        uint256 GAS_FOR_CALLBACK = 200000;
+        oraclize_newRandomDSQuery(
+            QUERY_EXECUTION_DELAY,
+            NUM_RANDOM_BYTES_REQUESTED,
+            GAS_FOR_CALLBACK
+        );
+        emit LogNewProvableQuery("Provable query was sent, standing by for the answer...");
+    }*/
 }
