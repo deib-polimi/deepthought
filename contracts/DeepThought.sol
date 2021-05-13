403ca0fc7f7921838c7ea5e92db5249482d658a5
Golfetto Thomas <tgolfetto@alpenite.com>
2021-05-13 12:35:05 +0200

    a lot of stuff :) (scoreboard, distribution of rewards)

--- contracts/DeepThought.sol
+++ contracts/DeepThought.sol
@@ -115,21 +115,36 @@ contract Oracle /*is usingOraclize*/ {
         // Voters unsealed vote
         mapping(address => VoteOption) voters_unsealedVotes;
         
+        // Number of scores (to iterate scoreboard)
+        uint num_scoreboard;
+
         // Scoreboard: importance order
         address[] scoreboard;
         
         // Score for each voter
         mapping(address => uint) scores;
         
+        // Number of true voters (to iterate following list)
+        uint num_T_voters;
+
         // Voters submitting vote T
         address[] T_voters;
+
+        // Number of false voters (to iterate following list)
+        uint num_F_voters;
         
         // Voters submitting vote F
         address[] F_voters;
+
+        // Number of true certifiers (to iterate following list)
+        uint num_T_certifiers;
         
         //Certifiers submitting cert T
         address[] T_certifiers;
         
+        // Number of false certifiers (to iterate following list)
+        uint num_F_certifiers;
+
         //Certifiers submitting cert F
         address[] F_certifiers;
     }
@@ -213,6 +228,13 @@ contract Oracle /*is usingOraclize*/ {
         prop.certifier_stakes[msg.sender][_vote? VoteOption.True : VoteOption.False] += stake;
         prop.stakes_total += stake;
         prop.certificates[_vote? VoteOption.True : VoteOption.False] += normalize_certifier_vote_weight(msg.sender, _prop_id, _vote);
+        if(_vote){
+            prop.T_certifiers.push(msg.sender);
+            prop.num_T_certifiers++;
+        }else{
+            prop.F_certifiers.push(msg.sender);
+            prop.num_F_certifiers++;
+        }
     }
     
     // Put your stake to receive a random proposition
@@ -254,10 +276,9 @@ contract Oracle /*is usingOraclize*/ {
         require(prop.status == PropositionStatus.VotingClose, "Proposition is not in the reveal phase!");
         require(prop.submitter == msg.sender, "This is not your proposition, you can't close it");
         stop_revealing_proposition(_prop_id);
-        //TODO: calc votes total using the unsealed_votes map normalizing them and populating the prop.votes map with the totals
-        //TODO: check the result of the proposition using prop.votes and prop.certificates maps and populate the prop.decision variable
-        //TODO: calculate the scoreboard (assign_score function to be used)
-        //TODO: distribute the rewards iterating the scoreboard (get_voter_reward and get_certifier_reward to be used)
+        set_decision(_prop_id);
+        create_scoreboard(_prop_id);
+        distribute_rewards(_prop_id);
         distribute_reputation(_prop_id);
     }
 
@@ -269,12 +290,19 @@ contract Oracle /*is usingOraclize*/ {
         if(hashedVote == keccak256(abi.encodePacked(_prop_id, VoteOption.True, _salt))){
             // Vote was true
             prop.voters_unsealedVotes[msg.sender] = VoteOption.True;
+            prop.num_T_voters++;
+            prop.T_voters.push(msg.sender);
+            prop.votes[VoteOption.True] += normalize_voter_vote_weight(msg.sender, _prop_id);
         }else{
             // Vote was false
             prop.voters_unsealedVotes[msg.sender] = VoteOption.False;
+            prop.num_F_voters++;
+            prop.F_voters.push(msg.sender);
+            prop.votes[VoteOption.False] += normalize_voter_vote_weight(msg.sender, _prop_id);
         }
-        //TODO: create the num_revealed_votes in the proposition structure,
-        // if this number is equal to the voters close the revealing phase automatically
+        if(prop.num_F_voters + prop.num_T_voters == prop.num_voters){
+            stop_revealing_proposition(_prop_id);
+        }
     }
  
     // ### INTERNAL FUNCTIONS 
@@ -313,7 +341,78 @@ contract Oracle /*is usingOraclize*/ {
                 decrement_reputation(cert_addr);
             }
         }
-    }    
+    }  
+
+    // Distribute the rewards to all the voters and certifiers
+    function distribute_rewards(uint256 _prop_id) internal {
+        Proposition storage prop = propositions[_prop_id];
+        require(prop.status == PropositionStatus.RevealingClose);
+        for(uint i = 0; i < prop.num_scoreboard; i++){
+            address addr = prop.scoreboard[i];
+            if(prop.voters_stakes[addr] > 0){
+                balances[addr] = get_voter_reward(addr, _prop_id);
+            }else{
+                balances[addr] = get_certifier_reward(addr, _prop_id);
+            }
+        }
+    } 
+
+    // Set the decision based on the vote majority
+    function set_decision(uint256 _prop_id) internal {
+        Proposition storage prop = propositions[_prop_id];
+        require(prop.status == PropositionStatus.RevealingClose);
+        VoteOption voter_result = VoteOption.Unknown;
+        VoteOption cert_result = VoteOption.Unknown;
+        if(prop.votes[VoteOption.True] > prop.votes[VoteOption.False]) voter_result = VoteOption.True;
+        if(prop.votes[VoteOption.False] > prop.votes[VoteOption.True]) voter_result = VoteOption.False;
+        if(prop.certificates[VoteOption.True] > prop.certificates[VoteOption.False]) cert_result = VoteOption.True;
+        if(prop.certificates[VoteOption.False] > prop.certificates[VoteOption.True]) cert_result = VoteOption.False;
+        if(voter_result == cert_result){
+            prop.decision = cert_result;
+        }else{
+            prop.decision = VoteOption.Unknown;
+        }
+    }  
+
+    // Create the scoreboard
+    function create_scoreboard(uint256 _prop_id) internal {
+        Proposition storage prop = propositions[_prop_id];
+        require(prop.status == PropositionStatus.RevealingClose);  
+        require(prop.decision == VoteOption.True || prop.decision == VoteOption.False || prop.decision == VoteOption.Unknown);
+        for(uint i = 0; i < prop.num_T_voters; i++){
+           address voter_addr = prop.T_voters[i];
+           prop.num_scoreboard++;
+           prop.scoreboard.push(voter_addr);
+           prop.scores[voter_addr] = assign_score(_prop_id, voter_addr);
+        }
+        for(uint j = 0; j < prop.num_F_voters; j++){
+           address voter_addr = prop.F_voters[j];
+           prop.num_scoreboard++;
+           prop.scoreboard.push(voter_addr);
+           prop.scores[voter_addr] = assign_score(_prop_id, voter_addr);           
+        }
+        for(uint k = 0; k < prop.num_T_certifiers; k++){
+           address cert_addr = prop.T_certifiers[k];
+           prop.num_scoreboard++;
+           prop.scoreboard.push(cert_addr);
+           prop.scores[cert_addr] = assign_score(_prop_id, cert_addr);
+        }
+        for(uint l = 0; l < prop.num_F_certifiers; l++){
+           address cert_addr = prop.F_certifiers[l];
+           prop.num_scoreboard++;
+           prop.scoreboard.push(cert_addr);
+           prop.scores[cert_addr] = assign_score(_prop_id, cert_addr);
+        }
+        order_scoreboard(_prop_id);
+    } 
+
+    // Order scoreboard to get highest scores first
+    function order_scoreboard(uint256 _prop_id) internal{
+        Proposition storage prop = propositions[_prop_id];
+        require(prop.status == PropositionStatus.RevealingClose);  
+        require(prop.decision == VoteOption.True || prop.decision == VoteOption.False || prop.decision == VoteOption.Unknown);
+        quickSort(prop.scoreboard, int(0), int(prop.scoreboard.length - 1), _prop_id);
+    }   
 
     // Calculate the minimum stake for a voter
     function get_min_voting_stake(address _voter) internal view returns (uint) {
@@ -368,9 +467,9 @@ contract Oracle /*is usingOraclize*/ {
     }
 
     // Calculate the reward of a certifier for a proposition
-    function get_certifier_reward(address _certifier, uint256 _prop_id, bool _outcome) internal view returns(uint){
+    function get_certifier_reward(address _certifier, uint256 _prop_id) internal view returns(uint){
         Proposition storage p = propositions[_prop_id];
-        uint256 stake = p.certifier_stakes[_certifier][_outcome ? VoteOption.True : VoteOption.False];
+        uint256 stake = p.certifier_stakes[_certifier][p.decision];
         uint reputation = reputations[_certifier];
         return beta * (stake ** 2) + (100 - beta) * (stake + reputation);
         // TODO: something different for certifiers
@@ -383,23 +482,62 @@ contract Oracle /*is usingOraclize*/ {
     }
     
     // Generate the scoreboard for a proposition
-    function assign_score(uint _prop_id, address _voter, bool _outcome) internal view returns(uint){
+    function assign_score(uint _prop_id, address _voter) internal view returns(uint){
         Proposition storage prop = propositions[_prop_id];
         require(prop.status == PropositionStatus.VotingClose);
         uint pred_score;
         uint info_score;
         uint q = prop.prediction_cert[_voter];
-        if(_outcome)
+        if(prop.decision == VoteOption.True)
         {
             pred_score = prop.voters_unsealedVotes[_voter]==VoteOption.True ? 200 * q - q ** 2 : 10000 - q ** 2;
         }
-        else
+        else if(prop.decision == VoteOption.False)
         {
             pred_score = prop.voters_unsealedVotes[_voter]==VoteOption.False ? 200 * q - q ** 2 : 10000 - q ** 2;
         }
         info_score = 10000 - (prediction_mean(_voter, prop) - prop.prediction_cert[_voter]) ** 2;
         return pred_score + info_score;
     }
+
+    // Produce the geometric mean without the predicion of the voter itself
+    function prediction_mean(address _voter, Proposition storage _prop) internal view returns(uint){
+        uint8 i;
+        uint tot = 1;
+        uint num;
+        if(_prop.voters_unsealedVotes[_voter] == VoteOption.True){
+            //TODO: check vote
+            num = _prop.T_voters.length;
+            for(i = 0; i < num; i ++){
+                tot *= _prop.prediction_cert[_prop.T_voters[i]];
+            }
+        }else{
+            num = _prop.F_voters.length;
+            for(i = 0; i < num; i ++){
+               tot *= _prop.prediction_cert[_prop.F_voters[i]];
+            }
+        }
+        tot /= _prop.prediction_cert[_voter];
+        return nthRoot(tot, num, 0, 1000);
+    }
+    
+    // Increment the reputation of a voter
+    function increment_reputation(address _voter) internal {
+        uint rep = reputations[_voter];
+        if (rep < max_reputation) {
+            reputations[_voter] += 1;
+        }
+    }
+
+    // Decrement the reputation of a voter
+    function decrement_reputation(address _voter) internal {
+        uint rep = reputations[_voter];
+        if (rep > 1) {
+            reputations[_voter] -= 1;
+        }
+    }
+
+    // ### UTILITY FUNCTIONS
 
     // Generate a pseudo-random number from 0 to _max
     function random(uint _max) internal view returns (uint8) 
@@ -442,42 +580,28 @@ contract Oracle /*is usingOraclize*/ {
         // Round to nearest in the last dp.
         return (xNew + 5) / 10;
     }
-    
-    // Produce the geometric mean without the predicion of the voter itself
-    function prediction_mean(address _voter, Proposition storage _prop) internal view returns(uint){
-        uint8 i;
-        uint tot = 1;
-        uint num;
-        if(_prop.voters_unsealedVotes[_voter] == VoteOption.True){
-            //TODO: check vote
-            num = _prop.T_voters.length;
-            for(i = 0; i < num; i ++){
-                tot *= _prop.prediction_cert[_prop.T_voters[i]];
-            }
-        }else{
-            num = _prop.F_voters.length;
-            for(i = 0; i < num; i ++){
-               tot *= _prop.prediction_cert[_prop.F_voters[i]];
-            }
-        }
-        tot /= _prop.prediction_cert[_voter];
-        return nthRoot(tot, num, 0, 1000);
-    }
-    
-    // Increment the reputation of a voter
-    function increment_reputation(address _voter) internal {
-        uint rep = reputations[_voter];
-        if (rep < max_reputation) {
-            reputations[_voter] += 1;
-        }
-    }
 
-    // Decrement the reputation of a voter
-    function decrement_reputation(address _voter) internal {
-        uint rep = reputations[_voter];
-        if (rep > 1) {
-            reputations[_voter] -= 1;
+    // Quicksorting scoreboard addresses comparing their scores
+    function quickSort(address[] memory _arr, int _left, int _right, uint256 _prop_id) internal{
+        int i = _left;
+        int j = _right;
+        if(i==j) return;
+        Proposition storage prop = propositions[_prop_id];
+        address pivot = _arr[uint(_left + (_right - _left) / 2)];
+        uint pivot_score = prop.scores[pivot];
+        while (i <= j) {
+            while (prop.scores[_arr[uint(i)]] < pivot_score) i++;
+            while (pivot_score < prop.scores[_arr[uint(j)]]) j--;
+            if (i <= j) {
+                (_arr[uint(i)], _arr[uint(j)]) = (_arr[uint(j)], _arr[uint(i)]);
+                i++;
+                j--;
+            }
         }
+        if (_left < j)
+            quickSort(_arr, _left, j, _prop_id);
+        if (i < _right)
+            quickSort(_arr, i, _right, _prop_id);
     }
 
 }
