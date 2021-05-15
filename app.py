e89c31b2b423fa605914f844bed7ddf736f40db7
marcoDige <marco1.digennaro@mail.polimi.it>
2021-05-15 20:36:06 +0200

    python script implementation updates

--- app.py
+++ app.py
@@ -1,41 +1,73 @@
-import json
-from web3 import Web3, HTTPProvider
+import os
+import sys
 
-# truffle development blockchain address
-ganache_blockchain_address = 'http://127.0.0.1:7545'
-# Path to the compiled contract JSON file
-compiled_contract_path = 'build/contracts/DeepThought.json'
-# Deployed contract address (see `migrate` command output: `contract address`)
-deployed_contract_address = '0x6A36E68ff85af58d84b311AdbBA83201A9Ea154E'
+from web3 import Web3
+import setup
 
-# Client instance to interact with the blockchain
-web3 = Web3(HTTPProvider(ganache_blockchain_address))
-# Set the default account (so we don't need to set the "from" for every transaction call)
-web3.eth.defaultAccount = web3.eth.accounts[0]
+def main():
 
-with open(compiled_contract_path) as file:
-    contract_json = json.load(file)  # load contract info as JSON
-    contract_abi = contract_json['abi']  # fetch contract's abi - necessary to call its functions
+    # SMART CONTRACT SETUP
 
-# Fetch deployed contract reference
-contract = web3.eth.contract(address=deployed_contract_address, abi=contract_abi)
+    print("DEEP THOUGHT")
 
-# Transact contract function (persisted to the blockchain). For every write function use "transact"
-message = contract.functions.subscribe().transact()
+    web3, contract = setup.init()
 
-print(message)
+    # Set the default account (so we don't need to set the "from" for every transaction call)
+    index = int(input('insert your account index: '))
+    web3.eth.defaultAccount = web3.eth.accounts[index]
 
-# Call contract function (this is not persisted to the blockchain)
-message = contract.functions.get_balance().call()
+    # CLI
 
-print(message)
+    while 1:
+        
+        insert = int(input('MAIN PAGE - HOW DO YOU WANT TO CONTINUE?\n1 - subscribe\n2 - sign in\n3 - quit\n'))
 
-# Call contract function (this is not persisted to the blockchain)
-message = contract.functions.get_reputation().call()
+        if insert == 3:
 
-print(message)
+            break
 
-# To crate a sealed vote circa ????
-#prop_id = "0"
-#salt = "42"
-#hashedVote = Web3.solidityKeccak(['bytes32'], prop_id + "VoteOption.True" + salt)
+        elif insert == 1:
+
+            # Transact contract function (persisted to the blockchain). For every write function use "transact"
+            contract.functions.subscribe().transact()
+            print("now you are subscribed")
+        
+        else :
+
+            while 1:
+
+                insert = int (input ('WHO YOU ARE?\n1 - certifier\n2 - voter\n3 - submitter\n4 - go back to main page\n'))
+
+                if insert == 4:
+
+                    break
+
+                elif insert == 1:
+
+                    print(web3.fromWei(int(contract.functions.get_balance().call()) * (10 ** 6), 'ether'))
+
+                elif input == 3:
+
+                    print(web3.fromWei(int(contract.functions.get_balance().call()) * (10 ** 6), 'ether'))
+
+                else :
+
+                    print(web3.fromWei(int(contract.functions.get_balance().call()) * (10 ** 6), 'ether'))
+
+            '''
+            # Debug utilities
+            # Call contract function (this is not persisted to the blockchain)
+            print(web3.fromWei(int(contract.functions.get_balance().call()) * (10 ** 6), 'ether'))
+
+            # Call contract function (this is not persisted to the blockchain)
+            print(contract.functions.get_reputation().call())
+            '''
+
+            # To crate a sealed vote circa ????
+            #prop_id = "0"
+            #salt = "42"
+            #hashedVote = Web3.solidityKeccak(['bytes32'], prop_id + "VoteOption.True" + salt)
+
+
+if __name__ == "__main__":
+    main()
