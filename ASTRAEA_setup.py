5cf94efda0074a7c1481641b34a864c9fe664226
Ita97 <lorenzo.ita@hotmail.com>
2021-10-21 21:25:21 +0200

    ASTRAEA_test: submit 100 prop and use 100 voters to vote

--- ASTRAEA_setup.py
+++ ASTRAEA_setup.py
@@ -6,8 +6,10 @@ ganache_blockchain_address = 'http://127.0.0.1:7545'
 # Path to the compiled contract JSON file
 compiled_contract_path = 'build/contracts/ASTRAEA.json'
 # Deployed contract address (see `migrate` command output: `contract address`)
-deployed_contract_address = '0x4587C0125dFB8e8874daF6b8A31117BE4211e1F4'
+deployed_contract_address = '0x6aEe96064Ba720cd58A218c7E52ea6cF091D5282'
 
+
+''' Use when constructor has not parameters '''
 def init():
 
     # Client instance to interact with the blockchain
@@ -19,4 +21,34 @@ def init():
 
     # Fetch deployed contract reference
     contract = web3.eth.contract(address=deployed_contract_address, abi=contract_abi)
+    return web3, contract
+
+
+''' Use it to pass the parameters to the constructor '''
+def set(closing_voting_stake, cert_target, voter_stake_max, certifier_stake_min):
+    # truffle development blockchain address
+    ganache_blockchain_address = 'http://127.0.0.1:7545'
+    # Path to the compiled contract JSON file
+    compiled_contract_path = 'build/contracts/ASTRAEA.json'
+    # Deployed contract address (see `migrate` command output: `contract address`)
+    # deployed_contract_address = '0x6aEe96064Ba720cd58A218c7E52ea6cF091D5282'
+    # Client instance to interact with the blockchain
+    web3 = Web3(HTTPProvider(ganache_blockchain_address))
+    closing_voting_stake = 100
+    cert_target = 10
+    voter_stake_max = 50
+    certifier_stake_min = 100
+
+    with open(compiled_contract_path) as file:
+        contract_json = json.load(file)  # load contract info as JSON
+        contract_abi = contract_json['abi']  # fetch contract's abi - necessary to call its functions
+        contract_bytecode = contract_json['bytecode']
+
+    # Fetch deployed contract reference
+    contract = web3.eth.contract(bytecode=contract_bytecode, abi=contract_abi)
+    web3.eth.defaultAccount = web3.eth.accounts[0]
+    tx_hash = contract.constructor(closing_voting_stake, cert_target, voter_stake_max, certifier_stake_min).transact()
+    tx_receipt = web3.eth.waitForTransactionReceipt(tx_hash)
+    address = tx_receipt.contractAddress
+    contract = web3.eth.contract(address=address, abi=contract_abi)
     return web3, contract
