6d9f83df366821efb9010c4cfb353205143b9dc4
marcoDige <marco1.digennaro@mail.polimi.it>
2022-07-07 16:44:40 +0200

    repo updated with final results and final dir org.

--- contracts/ASTRAEA.sol
+++ contracts/ASTRAEA.sol
@@ -10,8 +10,8 @@ contract ASTRAEA {
 
     /* ### PARAMETERS OF THE ORACLE ### */
 
-    // Stake enough to close the proposition (Dv)
-    uint closing_voting_stake;
+    // Maximum number of voters (number of voters to close the proposition)
+    uint max_n_votes;
 
     // Maximum voter stake
     uint voter_stake_max;
@@ -20,16 +20,16 @@ contract ASTRAEA {
     uint certifier_stake_min;
 
     // Reward pool for paying certifiers, splitted into True and False (Rt/Rf)
-    mapping (VoteOption => uint) cert_reward_pool;
+    mapping (VoteOption => uint) certifier_reward_pool;
 
     // Number of certification the reward_pool should have founds for (Tao)
-    uint cert_target;
+    uint tao;
 
     // List of all the prop_id
     uint256[] propositions_list;
 
     // List of closed/revealing prop_id
-    uint256[] closed_revealing_propositions_list;
+    uint256[] not_open_propositions_list;
 
     // Map of all the prop_id > proposition
     mapping (uint256 => Proposition) proposition;
@@ -38,10 +38,10 @@ contract ASTRAEA {
     mapping (address => uint256) balance;
 
     // Voter > prop_id > stake to check if he staked something so he can see which proposition he was assign to
-    mapping (address => mapping(uint256 => uint256)) ask_to_vote_stake;
+    mapping (address => mapping(uint256 => uint256)) vote_stake_request;
 
     // Certifier > stake to check if he staked something so he can see the proposition list
-    mapping (address => uint256) ask_to_certify_stake;
+    mapping (address => uint256) certifier_stake_request;
 
     // Voter > list of voted proposition
     mapping (address => uint256[]) voted_propositions;
@@ -52,13 +52,13 @@ contract ASTRAEA {
     // Voter > list of submitted proposition
     mapping (address => uint256[]) submitted_propositions;
 
-    constructor(uint _voter_stake_max, uint _cert_stake_min, uint _close_stake, uint _tao){
-        /*closing_voting_stake = 10;
-        cert_target = 10;
-        voter_stake_max = 10;
-        certifier_stake_min = 20;*/
-        closing_voting_stake = _close_stake;
-        cert_target = _tao;
+    constructor(uint _voter_stake_max, uint _cert_stake_min, uint _max_n_votes, uint _tao){
+        /*max_n_votes = ;
+        tao = ;
+        voter_stake_max = ;
+        certifier_stake_min = ;*/
+        max_n_votes = _max_n_votes;
+        tao = _tao;
         voter_stake_max = _voter_stake_max;
         certifier_stake_min = _cert_stake_min;
     }
@@ -69,6 +69,18 @@ contract ASTRAEA {
     enum VoteOption {Unknown, True, False}
 
     enum PropositionStatus {Open, Reveal, Close}
+
+    struct Vote {
+
+        VoteOption vote_unsealed;
+
+        bytes32 vote_sealed;
+
+        uint256 stake;
+
+        bool revealed;
+
+    }
 
     struct Proposition {
         // Unique proposition ID
@@ -92,41 +104,35 @@ contract ASTRAEA {
         // The status of the proposition Open > votingClosed > RevealClosed
         PropositionStatus status;
 
+        // VoteOption > sum of votes weight for the specific VoteOption
+        mapping (VoteOption => uint256) vote_balance;
+
         // Decided outcome
         VoteOption outcome;
+
+        // --- VOTERS VARIABLES ---
 
         // Total voting stake
         uint256 voters_stake_pool;
 
-        // Voters stake divided by the opinion
-        mapping (VoteOption => uint256) votes;
+        // Array of votes
+        Vote[] votes;
 
-        // List of the addresses who voted
-        address[] voters_list;
+        // voter > index of the voter's votes in votes array 
+        mapping (address => uint256[]) voted_indexes;
+
+        address[] voters;
 
         // Voters' stake (after reveal)
         mapping(address => mapping (VoteOption => uint256)) voter_stake;
 
-        // Voters' stake of each vote (before reveal)
-        mapping(address => mapping(uint256 => uint256)) voter_stake_per_vote;
+        // Votes submitting vote T
+        uint256[] T_vote_indexes;
+        
+        // Votes submitting vote F
+        uint256[] F_vote_indexes;
 
-        // Voters sealed vote (it supports multiple votes for a proposition)
-        mapping(address => mapping(uint => bytes32)) voter_sealedVote;
-
-        // Voters unsealed vote
-        mapping(address => mapping(uint => VoteOption)) voter_unsealedVote;
-
-        // Number of votes for each voter
-        mapping(address => uint8) voter_votes_number;
-
-        // Votes' identifier list for each voter
-        mapping(address => uint[]) voter_votes_id;
-
-        // Voters submitting True vote
-        address[] T_voters;
-
-        // Voters submitting False vote
-        address[] F_voters;
+        // --- CERTIFIERS VARIABLES ---
 
         // Total certificating stake
         uint256 certifiers_stake_pool;
@@ -174,12 +180,12 @@ contract ASTRAEA {
     }
 
     function get_number_propositions() public view returns (uint256){
-        require (ask_to_certify_stake[msg.sender] > 0, "Not a certifier! Make a request");
+        require (certifier_stake_request[msg.sender] > 0, "Not a certifier! Make a request");
         return propositions_list.length;
     }
 
     function get_prop_id_by_index(uint256 _i) public view returns (uint256){
-        require (ask_to_certify_stake[msg.sender] > 0, "Not a certifier! Make a request");
+        require (certifier_stake_request[msg.sender] > 0, "Not a certifier! Make a request");
         require (_i < propositions_list.length, "Out of bound");
         return propositions_list[_i];
     }
@@ -196,12 +202,8 @@ contract ASTRAEA {
         return voted_propositions[msg.sender].length;
     }
 
-    function get_voter_votes_number_by_prop_id(uint256 _prop_id) public view returns(uint8){
-        return proposition[_prop_id].voter_votes_number[msg.sender];
-    }
-
-    function get_vote_id_by_prop_id(uint256 _prop_id, uint8 index) public view returns(uint256){
-        return proposition[_prop_id].voter_votes_id[msg.sender][index];
+    function get_n_voted_times(uint256 _prop_id) public view returns (uint256){
+        return proposition[_prop_id].voted_indexes[msg.sender].length;
     }
 
     function get_number_submitted_propositions() public view returns (uint256){
@@ -295,7 +297,7 @@ contract ASTRAEA {
         require (_stake <= voter_stake_max, "The stake is too high");
         require (balance[msg.sender] >= _stake, "Not enough money to vote");
         uint256 prop_id = find_random_proposition();
-        ask_to_vote_stake[msg.sender][prop_id] = _stake;
+        vote_stake_request[msg.sender][prop_id] = _stake;
         balance[msg.sender] -= _stake;
 
         //To return a value from a state changer function we emit an event
@@ -303,54 +305,59 @@ contract ASTRAEA {
     }
 
     // Vote for the proposition you received
-    function vote(uint256 _prop_id, bytes32 _hashed_vote, uint _vote_id) public {
-        require (ask_to_vote_stake[msg.sender][_prop_id] > 0, "Not a voter of that proposition! Make a request");
+    function vote(uint256 _prop_id, bytes32 _hashed_vote) public {
+        require (vote_stake_request[msg.sender][_prop_id] > 0, "Not a voter of that proposition! Make a request");
+
         // Get the proposition
         Proposition storage prop = proposition[_prop_id];
         require(prop.status == PropositionStatus.Open, "Voting phase is closed!");
+
         // Move the stake to the proposition and vote
-        uint256 stake = ask_to_vote_stake[msg.sender][_prop_id];
-        ask_to_vote_stake[msg.sender][_prop_id] = 0;
-        prop.voters_list.push(msg.sender);
-        prop.voter_sealedVote[msg.sender][_vote_id] = _hashed_vote;
-        prop.voter_unsealedVote[msg.sender][_vote_id] = VoteOption.Unknown;
-        prop.voter_votes_number[msg.sender] += 1;
-        prop.voter_votes_id[msg.sender].push(_vote_id);
-        prop.voter_stake_per_vote[msg.sender][_vote_id] = stake;
+        uint256 stake = vote_stake_request[msg.sender][_prop_id];
+        vote_stake_request[msg.sender][_prop_id] = 0;
+        prop.votes.push(Vote(VoteOption.Unknown, _hashed_vote, stake, false));
+        prop.voted_indexes[msg.sender].push(prop.votes.length - 1);
         prop.voters_stake_pool += stake;
+
+        if(prop.voted_indexes[msg.sender].length == 1){
+            voted_propositions[msg.sender].push(_prop_id);
+            prop.voters.push(msg.sender);
+        }
+
         // If the max stake for a proposition is reached, the voting phase is closed
-        if (prop.voters_stake_pool >= closing_voting_stake){
+        if (prop.votes.length >= max_n_votes){
             close_proposition(_prop_id);
         }
 
-        voted_propositions[msg.sender].push(_prop_id);
     }
 
     // Reveal the voter's vote for a proposition
-    function reveal_voter_sealed_vote(uint256 _prop_id, uint _vote_id, string memory _salt) public {
+    function reveal_voter_sealed_vote(uint256 _prop_id, string memory _salt,  uint index) public {
         Proposition storage prop = proposition[_prop_id];
         require(prop.status == PropositionStatus.Reveal, "Proposition is not in the reveal phase!");
-        bytes32 hashed_vote = prop.voter_sealedVote[msg.sender][_vote_id];
-        require(hashed_vote != "", "Vote already revealed!");
-        uint stake = prop.voter_stake_per_vote[msg.sender][_vote_id];
+        bytes32 hashed_vote = prop.votes[prop.voted_indexes[msg.sender][index]].vote_sealed;
+        require(prop.votes[prop.voted_indexes[msg.sender][index]].revealed == false, "Vote already revealed!");
+        
+        uint stake = prop.votes[prop.voted_indexes[msg.sender][index]].stake;
         if(hashed_vote == keccak256(abi.encodePacked(_prop_id, true, _salt))){
             // Vote was true
-            prop.voter_unsealedVote[msg.sender][_vote_id] = VoteOption.True;
-            prop.T_voters.push(msg.sender);
-            prop.votes[VoteOption.True] += stake;
+            prop.votes[prop.voted_indexes[msg.sender][index]].vote_unsealed = VoteOption.True;
+            prop.T_vote_indexes.push(prop.voted_indexes[msg.sender][index]);
+            prop.vote_balance[VoteOption.True] += stake;
             prop.voter_stake[msg.sender][VoteOption.True] += stake;
+
         }else if(hashed_vote == keccak256(abi.encodePacked(_prop_id, false, _salt))){
             // Vote was false
-            prop.voter_unsealedVote[msg.sender][_vote_id] = VoteOption.False;
-            prop.F_voters.push(msg.sender);
-            prop.votes[VoteOption.False] += stake;
+            prop.votes[prop.voted_indexes[msg.sender][index]].vote_unsealed = VoteOption.False;
+            prop.F_vote_indexes.push(prop.voted_indexes[msg.sender][index]);
+            prop.vote_balance[VoteOption.False] += stake;
             prop.voter_stake[msg.sender][VoteOption.False] += stake;
         }else{
             revert("Wrong salt!!!");
         }
-        prop.voter_stake_per_vote[msg.sender][_vote_id] = 0;
-        prop.voter_sealedVote[msg.sender][_vote_id] = "";
-        if(prop.F_voters.length + prop.T_voters.length == prop.voters_list.length){
+
+        prop.votes[prop.voted_indexes[msg.sender][index]].revealed = true;
+        if(prop.F_vote_indexes.length + prop.T_vote_indexes.length == prop.votes.length){
             elaborate_result_proposition(_prop_id);
         }
     }
@@ -361,8 +368,8 @@ contract ASTRAEA {
         require(prop.status == PropositionStatus.Close, "Should be Close");
         VoteOption voter_result = VoteOption.Unknown;
         VoteOption cert_result = VoteOption.Unknown;
-        if(prop.votes[VoteOption.True] > prop.votes[VoteOption.False]) voter_result = VoteOption.True;
-        if(prop.votes[VoteOption.False] > prop.votes[VoteOption.True]) voter_result = VoteOption.False;
+        if(prop.vote_balance[VoteOption.True] > prop.vote_balance[VoteOption.False]) voter_result = VoteOption.True;
+        if(prop.vote_balance[VoteOption.False] > prop.vote_balance[VoteOption.True]) voter_result = VoteOption.False;
         if(prop.certificates[VoteOption.True] > prop.certificates[VoteOption.False]) cert_result = VoteOption.True;
         if(prop.certificates[VoteOption.False] > prop.certificates[VoteOption.True]) cert_result = VoteOption.False;
 
@@ -373,22 +380,22 @@ contract ASTRAEA {
 
     // Put your stake to be able to view the proposition as a certifier
     function certification_request(uint256 _stake) public {
-        require (ask_to_certify_stake[msg.sender] == 0, "Action already performed! Choose a proposition");
+        require (certifier_stake_request[msg.sender] == 0, "Action already performed! Choose a proposition");
         require (balance[msg.sender] >= _stake, "Not enough money to certify");
         require (_stake >= certifier_stake_min, "The stake is not enough!");
-        ask_to_certify_stake[msg.sender] = _stake;
+        certifier_stake_request[msg.sender] = _stake;
         balance[msg.sender] -= _stake;
     }
 
     // A certifier send his vote for a proposition
     function certify_proposition(uint256 _prop_id, bytes32 _hashed_vote) public {
-        require (ask_to_certify_stake[msg.sender] > 0, "Not a certifier! Make a request");
+        require (certifier_stake_request[msg.sender] > 0, "Not a certifier! Make a request");
         // Get the chosen proposition
         Proposition storage prop = proposition[_prop_id];
         // Increment the vote and move the stake to the proposition chosen
         prop.certifiers_list.push(msg.sender);
-        uint256 stake = ask_to_certify_stake[msg.sender];
-        ask_to_certify_stake[msg.sender] = 0;
+        uint256 stake = certifier_stake_request[msg.sender];
+        certifier_stake_request[msg.sender] = 0;
         prop.certifier_sealedVote[msg.sender] = _hashed_vote;
         prop.certifiers_stake_pool += stake;
         prop.certifier_stake[msg.sender][VoteOption.Unknown] += stake;
@@ -425,7 +432,7 @@ contract ASTRAEA {
     function elaborate_result_proposition(uint256 _prop_id) internal {
         stop_revealing_proposition(_prop_id);
         set_outcome(_prop_id);
-        //distribute_reward(_prop_id); Commented to run tests faster
+        distribute_reward(_prop_id); 
     }
 
      // Change status of proposition Open > VotingClosed
@@ -436,7 +443,7 @@ contract ASTRAEA {
 
         for(uint256 i = 0; i < propositions_list.length; i++){
             if(propositions_list[i] == _prop_id){
-                closed_revealing_propositions_list.push(propositions_list[i]);
+                not_open_propositions_list.push(propositions_list[i]);
                 propositions_list[i] = propositions_list[propositions_list.length - 1];
                 propositions_list.pop();
             }
@@ -452,8 +459,8 @@ contract ASTRAEA {
         for(uint i = 0; i< prop.certifiers_list.length; i++){
             uint stake = prop.certifier_stake[prop.certifiers_list[i]][VoteOption.Unknown];
             if (stake > 0){
-                cert_reward_pool[VoteOption.False] += stake/2;
-                cert_reward_pool[VoteOption.True] += stake/2;
+                certifier_reward_pool[VoteOption.False] += stake/2;
+                certifier_reward_pool[VoteOption.True] += stake/2;
                 prop.certifier_stake[prop.certifiers_list[i]][VoteOption.Unknown] = 0;
             }
         }
@@ -467,13 +474,12 @@ contract ASTRAEA {
         address addr;
 
         // voters
-        for (uint i = 0; i < prop.voters_list.length; i++){
-            addr = prop.voters_list[i];
-            if (prop.voter_earned_reward[addr] == 0){
-                reward = reward_voter(addr, _prop_id);
-                prop.voter_earned_reward[addr] = reward;
-                balance[addr] += reward;
-            }
+        for (uint i = 0; i < prop.voters.length; i++){
+            addr = prop.voters[i];
+            
+            reward = reward_voter(addr, _prop_id);
+            prop.voter_earned_reward[addr] = reward;
+            balance[addr] += reward;
 
         }
 
@@ -483,7 +489,7 @@ contract ASTRAEA {
             reward = reward_certifier(addr, _prop_id);
             prop.certifier_earned_reward[addr] = reward;
             balance[addr] += reward;
-            cert_reward_pool[prop.outcome] -= cert_reward_pool[prop.outcome]/cert_target;
+            certifier_reward_pool[prop.outcome] -= certifier_reward_pool[prop.outcome]/tao;
 
         }
 
@@ -497,12 +503,14 @@ contract ASTRAEA {
         require(prop.status == PropositionStatus.Close, "Should be Close");
         VoteOption outcome = prop.outcome;
         uint256 reward;
+        uint256 stake;
         VoteOption opposite = (outcome == VoteOption.True) ? VoteOption.False : VoteOption.True;
 
         // Voter is rewarded if his vote agrees with the outcome, eventual penalties go in the reward_pool
         if(outcome != VoteOption.Unknown){
-            reward = prop.voter_stake[_voter][outcome] * (prop.bounty / prop.votes[outcome] + 1);
-            cert_reward_pool[opposite] += prop.voter_stake[_voter][opposite];
+            stake = prop.voter_stake[_voter][outcome];
+            reward = stake + (stake * prop.bounty) / prop.vote_balance[outcome];
+            certifier_reward_pool[opposite] += prop.voter_stake[_voter][opposite];
         }
 
         // otherwise if the outcome is Unknown, gets back his stake
@@ -522,18 +530,20 @@ contract ASTRAEA {
         require(prop.status == PropositionStatus.Close, "Should be Close");
         VoteOption outcome = prop.outcome;
         uint256 reward;
+        uint256 stake;
         VoteOption opposite = (outcome == VoteOption.True) ? VoteOption.False : VoteOption.True;
 
         // Certifier is rewarded if the certification agrees with the outcome, eventual penalties and unclaimed bounties go in the reward_pool
         if(outcome != VoteOption.Unknown){
-            reward = prop.certifier_stake[_cert][outcome] * (cert_reward_pool[outcome] / (cert_target * prop.certificates[outcome]) + 1);
-            cert_reward_pool[opposite] += prop.certifier_stake[_cert][opposite] + prop.certifier_stake[_cert][VoteOption.Unknown];
+            stake = prop.certifier_stake[_cert][outcome];
+            reward = (stake * certifier_reward_pool[outcome]) / (tao * prop.certificates[outcome]) + stake;
+            certifier_reward_pool[opposite] += prop.certifier_stake[_cert][opposite] + prop.certifier_stake[_cert][VoteOption.Unknown];
         }
 
         // otherwise if the outcome is Unknown, loses everything
         else{
             reward = 0;
-            cert_reward_pool[opposite] += cert_reward_pool[outcome]/cert_target;
+            certifier_reward_pool[opposite] += certifier_reward_pool[outcome]/tao;
         }
 
         prop.certifier_stake[_cert][VoteOption.Unknown] = 0;
--- results/get_result.py
+++ results/get_result.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 columns = ['voters', 'propositions', 'accuracy', 'adv_control', 'prop_corrupted', 'target_corrupted', 'elapsed_time']
-df = pd.read_csv('results_DeepThought.csv')
+df = pd.read_csv('results_ASTRAEA.csv')
 df = df.drop(0)
 print (df)
 
@@ -98,6 +98,82 @@ print('num_test : ', num)
 
 print("\nCASE 6:")
 case = df.loc[df['accuracy'] == 0.95].loc[df['adv_control'] == 0.25]
+#print(case)
+num = case.shape[0]
+perc_target_corrupted = case['target_corrupted'].sum()/num
+avg_corrupted = case['prop_corrupted'].sum()/num
+std_corrupted = case['prop_corrupted'].std()
+min_corrupted = case['prop_corrupted'].min()
+max_corrupted = case['prop_corrupted'].max()
+for i in range(4):
+	print(columns[i],":", case.iloc[0][i], end=", ")
+
+print('specific_target_corrupted_perc', perc_target_corrupted, end=", ")
+print('prop_corrupted_avg :', avg_corrupted, end=", ")
+print('prop_corrupted_std : ', std_corrupted, end=", ")
+print('prop_corrupted_min : ', min_corrupted, end=", ")
+print('prop_corrupted_max : ', max_corrupted, end=", ")
+print('num_test : ', num)
+
+print("\nCASE 7:")
+case = df.loc[df['accuracy'] == 0.80].loc[df['adv_control'] == 0.35]
+#print(case)
+num = case.shape[0]
+perc_target_corrupted = case['target_corrupted'].sum()/num
+avg_corrupted = case['prop_corrupted'].sum()/num
+std_corrupted = case['prop_corrupted'].std()
+min_corrupted = case['prop_corrupted'].min()
+max_corrupted = case['prop_corrupted'].max()
+for i in range(4):
+	print(columns[i],":", case.iloc[0][i], end=", ")
+
+print('specific_target_corrupted_perc', perc_target_corrupted, end=", ")
+print('prop_corrupted_avg :', avg_corrupted, end=", ")
+print('prop_corrupted_std : ', std_corrupted, end=", ")
+print('prop_corrupted_min : ', min_corrupted, end=", ")
+print('prop_corrupted_max : ', max_corrupted, end=", ")
+print('num_test : ', num)
+
+print("\nCASE 8:")
+case = df.loc[df['accuracy'] == 0.95].loc[df['adv_control'] == 0.35]
+#print(case)
+num = case.shape[0]
+perc_target_corrupted = case['target_corrupted'].sum()/num
+avg_corrupted = case['prop_corrupted'].sum()/num
+std_corrupted = case['prop_corrupted'].std()
+min_corrupted = case['prop_corrupted'].min()
+max_corrupted = case['prop_corrupted'].max()
+for i in range(4):
+	print(columns[i],":", case.iloc[0][i], end=", ")
+
+print('specific_target_corrupted_perc', perc_target_corrupted, end=", ")
+print('prop_corrupted_avg :', avg_corrupted, end=", ")
+print('prop_corrupted_std : ', std_corrupted, end=", ")
+print('prop_corrupted_min : ', min_corrupted, end=", ")
+print('prop_corrupted_max : ', max_corrupted, end=", ")
+print('num_test : ', num)
+
+print("\nCASE 9:")
+case = df.loc[df['accuracy'] == 0.80].loc[df['adv_control'] == 0.45]
+#print(case)
+num = case.shape[0]
+perc_target_corrupted = case['target_corrupted'].sum()/num
+avg_corrupted = case['prop_corrupted'].sum()/num
+std_corrupted = case['prop_corrupted'].std()
+min_corrupted = case['prop_corrupted'].min()
+max_corrupted = case['prop_corrupted'].max()
+for i in range(4):
+	print(columns[i],":", case.iloc[0][i], end=", ")
+
+print('specific_target_corrupted_perc', perc_target_corrupted, end=", ")
+print('prop_corrupted_avg :', avg_corrupted, end=", ")
+print('prop_corrupted_std : ', std_corrupted, end=", ")
+print('prop_corrupted_min : ', min_corrupted, end=", ")
+print('prop_corrupted_max : ', max_corrupted, end=", ")
+print('num_test : ', num)
+
+print("\nCASE 10:")
+case = df.loc[df['accuracy'] == 0.95].loc[df['adv_control'] == 0.45]
 #print(case)
 num = case.shape[0]
 perc_target_corrupted = case['target_corrupted'].sum()/num
